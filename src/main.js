afda0564e6dfbb1d10c723a6f2df5fd34f834db5
lqixianl <76685189+lqixianl@users.noreply.github.com>
2020-12-31 05:47:29 +0000

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,13 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import Axios from 'axios'
-import cors from 'cors';
+
 
 // import store from './store'
 Vue.config.productionTip = false
 Vue.prototype.$http = Axios;
 
-Vue.use(cors());
+
 
 new Vue({
   router,
